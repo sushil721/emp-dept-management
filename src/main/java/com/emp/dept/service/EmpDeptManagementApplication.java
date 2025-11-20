b17a8a124c3acf48bf4f92cd09f8316ef97048dc
sushil721 <singh.sushil721@gmail.com>
2025-11-21 00:06:11 +0530

    first commit

---
+++ src/main/java/com/emp/dept/service/EmpDeptManagementApplication.java
@@ -0,0 +1,13 @@
+package com.emp.dept.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDeptManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDeptManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emp/dept/service/EmpDeptManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.dept.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDeptManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
